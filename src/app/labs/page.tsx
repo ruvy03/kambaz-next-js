c70435b056ae8d73046adb9a6a9c0d285851b3de
ruvy03 <dhruv.sinha06@gmail.com>
2025-09-09 19:20:49 -0400

    layout with the TOC

---
+++ src/app/labs/page.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+export default function Lab1() {
+  return (
+    <div id="wd-labs">
+      <h2>Labs</h2>
+      <ul>
+        <li>
+          <Link href="/labs/lab1" id="wd-lab1-link">
+            Lab 1
+          </Link>
+        </li>
+        <li>
+          <Link href="/labs/lab2" id="wd-lab2-link">
+            Lab 2
+          </Link>
+        </li>
+        <li>
+          <Link href="/labs/lab3" id="wd-lab3-link">
+            Lab 3
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
