27b647d63902b4de2a1c94ce5fc15eedcce07e59
ruvy03 <dhruv.sinha06@gmail.com>
2025-09-09 22:38:29 -0400

    account screens

--- src/app/(kambaz)/page.tsx
+++ src/app/(kambaz)/page.tsx
@@ -1,7 +1,4 @@
+import { redirect } from "next/navigation";
 export default function Kambaz() {
-  return (
-    <div id="wd-kambaz">
-      <h1>Kambaz</h1>
-    </div>
-  );
+  redirect("/account/signin");
 }
