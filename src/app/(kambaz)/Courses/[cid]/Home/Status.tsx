fa82ed59571c1422b80dfdff6e007c9a41d33ab4
ruvy03 <dhruv.sinha06@gmail.com>
2025-09-15 17:06:32 -0400

    status better

--- src/app/(kambaz)/Courses/[cid]/Home/Status.tsx
+++ src/app/(kambaz)/Courses/[cid]/Home/Status.tsx
@@ -4,15 +4,17 @@ export default function CourseStatus() {
       <h2>Course Status</h2>
       <button>Unpublish</button> <button>Published</button>
       <br />
-      <br />
       <button>Import Existing Content</button>
       <br />
+      <button>Import from Commons</button>
+      <br />
+      <button>Choose Home Page</button>
       <br />
       <button>View Course Stream</button>
       <br />
-      <br />
       <button>New Announcement</button>
       <br />
+      <button>New Analytics</button>
       <br />
       <button>View Course Notifications</button>
     </div>
