4ec42cf15512d3c48ee7244861ee70b75ae0ce7a
ruvy03 <dhruv.sinha06@gmail.com>
2025-10-03 18:31:03 -0400

    navigation perfect

--- app/(Kambaz)/Account/Navigation.tsx
+++ app/(Kambaz)/Account/Navigation.tsx
@@ -1,28 +1,51 @@
-"use client";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { ListGroup, ListGroupItem } from "react-bootstrap";
-import { FaSignInAlt, FaUserCircle, FaUserPlus } from "react-icons/fa";
 
 export default function AccountNavigation() {
-  const pathname = usePathname();
-  const links = [
-    { label: "Signin", path: "/Account/Signin", icon: <FaSignInAlt /> },
-    { label: "Signup", path: "/Account/Signup", icon: <FaUserPlus /> },
-    { label: "Profile", path: "/Account/Profile", icon: <FaUserCircle /> },
-  ];
   return (
-    <ListGroup id="wd-account-navigation" style={{ width: 120 }}>
-      {links.map((link) => (
-        <ListGroupItem
-          key={link.path}
-          as={Link}
-          href={link.path}
-          active={pathname.includes(link.path)}
-        >
-          {link.icon} {link.label}
-        </ListGroupItem>
-      ))}
-    </ListGroup>
+    <div id="wd-account-navigation">
+      <Link
+        href="/Account/Signin"
+        style={{
+          textDecoration: "none",
+          fontSize: "1.05em",
+          paddingLeft: "10px",
+          paddingRight: "10px",
+          color: "black",
+          marginBottom: "2px",
+          display: "block",
+        }}
+      >
+        <span style={{ borderLeft: "1.5px solid black", paddingLeft: "5px" }}>
+          Signin
+        </span>
+      </Link>
+      <Link
+        href="/Account/Signup"
+        style={{
+          textDecoration: "none",
+          fontSize: "1.05em",
+          paddingLeft: "10px",
+          paddingRight: "10px",
+          color: "red",
+          marginBottom: "2px",
+          display: "block",
+        }}
+      >
+        <span style={{ paddingLeft: "6.5px" }}>Signup</span>
+      </Link>
+      <Link
+        href="/Account/Profile"
+        style={{
+          textDecoration: "none",
+          fontSize: "1.05em",
+          paddingLeft: "10px",
+          paddingRight: "10px",
+          color: "red",
+          display: "block",
+        }}
+      >
+        <span style={{ paddingLeft: "6.5px" }}>Profile</span>
+      </Link>
+    </div>
   );
 }
