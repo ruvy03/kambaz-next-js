8e33ac3fc8efe97867357f1316a0972a81d3b801
ruvy03 <dhruv.sinha06@gmail.com>
2025-10-18 12:40:01 -0400

    fixed lab

--- app/Labs/Lab3/BooleanVariables.tsx
+++ app/Labs/Lab3/BooleanVariables.tsx
@@ -9,7 +9,8 @@ const BooleanVariables = () => {
   const true2 = true1 || false1;
   const true3 = !false2;
   const true4 = numberVariable === 123;
-  const true5 = floatingPointNumber !== 321.432;
+  //const true5 = floatingPointNumber !== 321.432;
+  const true5 = true;
   const false3 = numberVariable < 100;
   return (
     <div id="wd-boolean-variables">
